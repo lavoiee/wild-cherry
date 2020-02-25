4c325f9e8276b8534189794a6c031c034322bd11
Eric <ericjohnlavoie@gmail.com>
2020-02-25 03:22:40 -0500

    fixed naming error in MyStringStore.sol that was preventing compile.

--- drizzle-react-tutorial/contracts/MyStringStore.sol
+++ drizzle-react-tutorial/contracts/MyStringStore.sol
@@ -1,7 +1,7 @@
 pragma solidity ^0.5.0;
 
 contract MyStringStore {
-  string public MyStringStore = "Hello World";
+  string public MyString = "Hello World";
 
   function set(string memory x) public {
     myString = x;
